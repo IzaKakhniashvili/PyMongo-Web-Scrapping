14cd1b602597d70fa40d97b944f4d1fef8588e40
Iza Kakhniashvili <152190428+IzaKakhniashvili@users.noreply.github.com>
2024-09-28 16:55:41 +0400

    Final mongo_data.py

--- mongo_data.py
+++ mongo_data.py
@@ -38,3 +38,30 @@ def get_average_steps(collection):
     ])
     avg_steps = list(total_steps)
     return avg_steps[0]['avgSteps'] if avg_steps else 0
+
+
+def get_most_recipes_author(collection):
+    most_recipes_author = collection.aggregate([
+        {"$group": {
+            "_id": "$author",
+            "recipe_count": {"$sum": 1}
+        }},
+        {"$sort": {"recipe_count": -1}},
+        {"$limit": 1}
+    ])
+
+    for author in most_recipes_author:
+        return author['_id'], author['recipe_count']
+    return None, None
+
+def get_most_portions_recipe(collection):
+    most_portions_recipe = collection.aggregate([
+        {"$match": {"portion": {"$ne": None}}},
+        {"$sort": {"portion": -1}},
+        {"$limit": 1}
+    ])
+
+    for recipe in most_portions_recipe:
+        return recipe['name'], recipe['url'], recipe['portion']
+    return None, None, None
+
